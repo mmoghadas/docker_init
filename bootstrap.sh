115576e9721f3af962c817bcac45ffd341167d8e
Mike <mike.moghadas@gmail.com>
2015-04-03 14:20:57 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,8 +17,7 @@ cat <<EOF > Vagrantfile
 Vagrant.configure(2) do |config|
   config.vm.box = "centos7"
   config.vm.box_url = "https://f0fff3908f081cb6461b407be80daf97f07ac418.googledrive.com/host/0BwtuV7VyVTSkUG1PM3pCeDJ4dVE/centos7.box"
-  config.vm.network "forwarded_port", guest: 2375, host: 2375
-  config.vm.network "forwarded_port", guest: 2376, host: 2376
+  config.vm.network "forwarded_port", guest: 2375, host: 2376
 
   config.vm.provider "virtualbox" do |v|
     v.name = "$VAGRANTNAME"
