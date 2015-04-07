bc5ad8b61b66587ab8984cdc55e519623c7a66e6
Mike <mike.moghadas@gmail.com>
2015-04-07 09:38:52 -0700

    Update cli.rb

--- lib/docker_init/cli.rb
+++ lib/docker_init/cli.rb
@@ -4,13 +4,13 @@ require 'docker_init/base'
 module DockerInit
   class Cli < Clamp::Command
 
-    option ["-d", "--home"], "VAGRANTHOME", "required: vagrant home", :required => true
+    option ["-d", "--vagranthome"], "VAGRANTHOME", "required: vagrant home", :required => true
     option ["-n", "--name"], "VAGRANTNAME", "required: vagrant name", :required => true
     option ["-h", "--hostname"], "HOSTNAME", "required if tls enabled"
     option "--tls", :flag, "USE TLS"
 
     def execute
-      DockerInit::Base.new.setup(home, name, {tls: tls?, hostname: hostname})
+      DockerInit::Base.new.setup(vagranthome, name, {tls: tls?, hostname: hostname})
     end
   end
 
